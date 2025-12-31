7c2fadfc7f3f315ddeb80afe8141b2a977930380
Palak Verma <palak.20253182@mnnit.ac.in>
2025-12-31 13:01:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -130,5 +130,10 @@ def analyze():
         return jsonify({'error': str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+#if __name__ == '__main__':
+#    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
